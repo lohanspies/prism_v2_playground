a3f7a53be6f511f9f04faed19f519d80f8e7f857
lohanspies <lohan.spies@didx.co.za>
2022-12-21 00:32:17 +0200

    Use local agent to generate openapi.yaml

--- openapi-generator/generate_dereferenced_openapi_spec.sh
+++ openapi-generator/generate_dereferenced_openapi_spec.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-openapi-generator-cli generate -i https://k8s-dev.atalaprism.io/docs/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
+#openapi-generator-cli generate -i https://k8s-dev.atalaprism.io/docs/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
+
+openapi-generator-cli generate -i http://127.0.0.1:8080/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
