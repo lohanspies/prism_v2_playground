69042af6cbfa69047081f099dc532ce6e8e96adc
AlexAndrei98 <alexandrei1998@hotmail.it>
2023-02-09 17:40:24 -0600

    add ts wrapper

---
+++ openapi-generator/prism-agent-ts-open-api/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './ConnectionsManagementApi';
+export * from './DIDApi';
+export * from './DIDRegistrarApi';
+export * from './IssueCredentialsProtocolApi';
+export * from './PresentProofApi';
+export * from './RevocationRegistryApi';
+export * from './SchemaRegistryApi';
+export * from './VerificationApi';
