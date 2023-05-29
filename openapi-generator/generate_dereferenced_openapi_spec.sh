95e71109dee49ab6f38b11e16105d0c895ab46f6
lohanspies <lohan.spies@didx.co.za>
2023-05-29 16:47:24 +0200

    update script to use local agent openapi spec URL

--- openapi-generator/generate_dereferenced_openapi_spec.sh
+++ openapi-generator/generate_dereferenced_openapi_spec.sh
@@ -7,12 +7,12 @@
 #
 # Example: 
 #
-#   openapi-generator-cli generate -i http://localhost:8090/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi yml --auth apiKey:<url encoded token>
+#openapi-generator-cli generate -i http://localhost:8080/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi yml --auth apiKey:<url encoded token>
 
 #openapi-generator-cli generate -i https://k8s-dev.atalaprism.io/docs/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
 
 #openapi-generator-cli generate -i http://127.0.0.1:8080/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
 
-openapi-generator-cli generate -i https://nizhm0j69lrg.atalaprism.io/docs/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
+#openapi-generator-cli generate -i https://nizhm0j69lrg.atalaprism.io/docs/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml
 
-#openapi-generator-cli generate -i http://localhost:8090/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml -a apiKey:kxr9i%406XgKBUxe%25O
+openapi-generator-cli generate -i http://localhost:8080/prism-agent/api/openapi-spec.yaml -g openapi-yaml -o ./derereferenced_openapi.yml --auth apiKey:kxr9i%406XgKBUxe%25O
